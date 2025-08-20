54c820ba894e89ff5d6f03a5cbf76be562fa15ac
Pim Snel <post@pimsnel.com>
2025-08-20 22:33:31 +0200

    add cowsay

--- flake.nix
+++ flake.nix
@@ -8,12 +8,14 @@
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.fortune = nixpkgs.legacyPackages.x86_64-linux.fortune;
+    packages.x86_64-linux.cowsay = nixpkgs.legacyPackages.x86_64-linux.cowsay;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.fortune;
 
     # Hydra jobset for building the fortune package
     hydraJobs = {
       fortune.x86_64-linux = self.packages.x86_64-linux.fortune;
+      cowsay.x86_64-linux = self.packages.x86_64-linux.cowsay;
     };
 
   };
